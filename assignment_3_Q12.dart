9301e41ed3bcb0e301589dc60bf93d10006b3a20
HuzaifJaamal <huzaifajamal95@gmail.com>
2023-08-11 11:08:55 +0500

    Assignment-3

--- assignment_3_Q12.dart
+++ assignment_3_Q12.dart
@@ -1,4 +1,4 @@
-// Q12 Write a program to display a pattern like a right angle triangle with a 
+// Q12 Write a program to display a pattern like a right angle triangle with a
 // number using loop.
 // The pattern like :
 // 1
@@ -6,3 +6,19 @@
 // 123
 // 1234
 
+import 'dart:io';
+
+void main() {
+  int numbersOfTimes = 4;
+
+  for (int i = 1; i <= numbersOfTimes; i++) {
+    // the variable j is used as a loop counter in the inner loop.
+    // It represents the number of asterisks to be printed in each row.
+    for (int j = 1; j <= i; j++) {
+      // print statement give you newline when printing again.
+      // while "stdout.write" doesn't give you the newline. it continou to print on the same line.
+      stdout.write(j);
+    }
+    print(''); // Print a new line after each row
+  }
+}
--- assignment_3_Q13.dart
+++ assignment_3_Q13.dart
@@ -1,7 +1,25 @@
-// Q13 Write a program to make such a pattern like a right angle triangle with 
+// Q13 Write a program to make such a pattern like a right angle triangle with
 // a number which will repeat a number in a row.
 // The pattern like :
 //  1
 //  22
 //  333
 //  4444
+
+import 'dart:io';
+
+void main() {
+  int numbersOfTimes = 4;
+
+  // the variable j is used as a loop counter in the inner loop.
+  // It represents the number of asterisks to be printed in each row.
+  for (int i = 1; i <= numbersOfTimes; i++) {
+    for (int j = 1; j <= i; j++) {
+      // print statement give you newline when printing again.
+      // while "stdout.write" doesn't give you the newline. it continou to print on the same line.
+      // print 'i' here because i want same number in row.
+      stdout.write(i);
+    }
+    print('');
+  }
+}
